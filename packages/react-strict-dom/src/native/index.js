20de8480428b77400b21057aadcd71df63f108fb
Naman Goel <nmn@users.noreply.github.com>
2024-03-06 00:52:35 -0800

    Override types for the StyleX shim

--- packages/react-strict-dom/src/native/index.js
+++ packages/react-strict-dom/src/native/index.js
@@ -7,11 +7,16 @@
  * @flow
  */
 
+import typeof * as TStyleX from '@stylexjs/stylex';
+
 import * as html from './html';
-import * as css from './stylex';
+import * as cssRaw from './stylex';
 import { ThemeProvider } from './modules/ThemeContext';
 import { typeof ThemeProvider as TThemeProvider } from './modules/ThemeContext';
 
 const contexts = { ThemeProvider: (ThemeProvider: TThemeProvider) };
+
+// Export using StyleX types as the shim has divergent types internally.
+const css: TStyleX = (cssRaw: $FlowFixMe);
 
 export { contexts, css, html };
