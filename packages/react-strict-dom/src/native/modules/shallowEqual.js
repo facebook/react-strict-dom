50399cc0508402261193489f0abab2b11a05b149
Royi Hagigi <royi@meta.com>
2024-08-22 15:53:11 -0400

    Stop including Object.is polyfill in shallowEquals

--- packages/react-strict-dom/src/native/modules/shallowEqual.js
+++ packages/react-strict-dom/src/native/modules/shallowEqual.js
@@ -4,7 +4,7 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  *
- * Copied from the fbjs implementation
+ * Copied from the fbjs implementation, but without Object.is polyfill
  *
  * @flow strict
  */
@@ -17,29 +17,12 @@
 const hasOwnProperty = Object.prototype.hasOwnProperty;
 
 /**
- * inlined Object.is polyfill to avoid requiring consumers ship their own
- * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/is
- */
-function is(x: mixed, y: mixed): boolean {
-  // SameValue algorithm
-  if (x === y) {
-    // Steps 1-5, 7-10
-    // Steps 6.b-6.e: +0 != -0
-    // Added the nonzero y check to make Flow happy, but it is redundant
-    return x !== 0 || y !== 0 || 1 / x === 1 / y;
-  } else {
-    // Step 6.a: NaN == NaN
-    return x !== x && y !== y;
-  }
-}
-
-/**
  * Performs equality by iterating through keys on an object and returning false
  * when any key has values which are not strictly equal between the arguments.
  * Returns true when the values of all keys are strictly equal.
  */
 export function shallowEqual(objA: mixed, objB: mixed): boolean {
-  if (is(objA, objB)) {
+  if (Object.is(objA, objB)) {
     return true;
   }
 
@@ -64,7 +47,7 @@ export function shallowEqual(objA: mixed, objB: mixed): boolean {
     if (
       !hasOwnProperty.call(objB, keysA[i]) ||
       // $FlowFixMe[incompatible-use]
-      !is(objA[keysA[i]], objB[keysA[i]])
+      !Object.is(objA[keysA[i]], objB[keysA[i]])
     ) {
       return false;
     }
