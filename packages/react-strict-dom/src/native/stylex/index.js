a5079f6285d90cbdff1aba83504a0938eed997ae
Jorge Cabiedes Acosta <jorgecab@meta.com>
2024-09-25 12:28:12 -0700

    prepare to remove experimental_ prefix from mix-blend-mode
    prepare to remove experimental_ prefix from mix-blend-mode
    
    prepare to remove experimental_ prefix from mix-blend-mode

--- packages/react-strict-dom/src/native/stylex/index.js
+++ packages/react-strict-dom/src/native/stylex/index.js
@@ -641,6 +641,7 @@ export function props(
     }
     // experimental styles
     else if (styleProp === 'mixBlendMode') {
+      nextStyle.mixBlendMode = styleValue;
       nextStyle.experimental_mixBlendMode = styleValue;
     }
     // Everything else
