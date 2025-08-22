d15c7a3277505510e0d05edff0c382a2af2d90dd
Nicolas Gallagher <necolas@meta.com>
2025-08-22 16:34:24 -0700

    Use component syntax

--- packages/react-strict-dom/src/native/modules/createStrictDOMComponent.js
+++ packages/react-strict-dom/src/native/modules/createStrictDOMComponent.js
@@ -35,184 +35,182 @@ export function createStrictDOMComponent<T, P: StrictProps>(
   tagName: string,
   defaultProps?: P
 ): component(ref?: React.RefSetter<T>, ...P) {
-  const component: React.AbstractComponent<P, T> = React.forwardRef(
-    function (props, forwardedRef) {
-      let NativeComponent =
-        tagName === 'button'
-          ? ReactNative.Pressable
-          : ReactNative.ViewNativeComponent;
-      const elementRef = useStrictDOMElement<T>(forwardedRef, { tagName });
-      const hasTextAncestor = React.useContext(ReactNative.TextAncestorContext);
+  const provideInheritableStyle =
+    tagName !== 'br' || tagName !== 'hr' || tagName !== 'option';
 
-      /**
-       * Resolve global HTML and style props
-       */
+  component Component(ref: React.RefSetter<T>, ...props: P) {
+    let NativeComponent =
+      tagName === 'button'
+        ? ReactNative.Pressable
+        : ReactNative.ViewNativeComponent;
+    const elementRef = useStrictDOMElement<T>(ref, { tagName });
+    const hasTextAncestor = React.useContext(ReactNative.TextAncestorContext);
 
-      const { customProperties, nativeProps, inheritableStyle } =
-        useNativeProps(defaultProps, props, {
-          provideInheritableStyle:
-            tagName !== 'br' || tagName !== 'hr' || tagName !== 'option',
-          withInheritedStyle: false,
-          withTextStyle: false
-        });
+    /**
+     * Resolve global HTML and style props
+     */
 
-      if (
-        nativeProps.onPress != null &&
-        NativeComponent === ReactNative.ViewNativeComponent
-      ) {
-        NativeComponent = ReactNative.Pressable;
+    const { customProperties, nativeProps, inheritableStyle } = useNativeProps(
+      defaultProps,
+      props,
+      {
+        provideInheritableStyle,
+        withInheritedStyle: false,
+        withTextStyle: false
       }
+    );
 
-      // Tag-specific props
-
-      if (tagName === 'button') {
-        nativeProps.role ??= 'button';
-      } else if (tagName === 'header') {
-        nativeProps.role ??= 'header';
-      } else if (tagName === 'li') {
-        nativeProps.role ??= 'listitem';
-      } else if (tagName === 'ol' || tagName === 'ul') {
-        nativeProps.role ??= 'list';
-      }
-
-      // Component-specific props
-
-      if (NativeComponent === ReactNative.Pressable) {
-        if (props.disabled === true) {
-          nativeProps.disabled = true;
-          nativeProps.focusable = false;
-        }
-      }
-
-      nativeProps.ref = elementRef;
-
-      // Workaround: React Native doesn't support raw text children of View
-      // Sometimes we can auto-fix this
-      if (typeof nativeProps.children === 'string') {
-        nativeProps.children = <TextString children={nativeProps.children} />;
-      }
-
-      // Polyfill for default of "display:block"
-      // which implies "displayInside:flow"
-      let nextDisplayInsideValue = 'flow';
-      const displayInsideValue = useDisplayInside();
-      const displayValue = nativeProps.style.display;
-
-      if (__DEV__) {
-        const nativeStyle = nativeProps.style;
-        if (displayInsideValue !== 'flex') {
-          // Error message if the element is not a flex child but tries to use flex
-          ['flex', 'flexBasis', 'flexGrow', 'flexShrink'].forEach(
-            (styleProp) => {
-              const value = nativeStyle[styleProp];
-              if (value != null) {
-                errorMsg(
-                  `"display:flex" is required on the parent for "${styleProp}" to have an effect.`
-                );
-              }
-            }
-          );
-        }
-      }
-
-      if (displayValue === 'flex') {
-        nextDisplayInsideValue = 'flex';
-        nativeProps.style.alignContent ??= 'stretch';
-        nativeProps.style.alignItems ??= 'stretch';
-        nativeProps.style.flexBasis ??= 'auto';
-        nativeProps.style.flexDirection ??= 'row';
-        nativeProps.style.flexShrink ??= 1;
-        nativeProps.style.flexWrap ??= 'nowrap';
-        nativeProps.style.justifyContent ??= 'flex-start';
-      } else if (displayValue === 'block' && displayInsideValue === 'flow') {
-        // Force the block emulation styles
-        nextDisplayInsideValue = 'flow';
-        nativeProps.style.alignItems = 'stretch';
-        nativeProps.style.display = 'flex';
-        nativeProps.style.flexBasis = 'auto';
-        nativeProps.style.flexDirection = 'column';
-        nativeProps.style.flexShrink = 0;
-        nativeProps.style.flexWrap = 'nowrap';
-        nativeProps.style.justifyContent = 'flex-start';
-      }
-
-      if (displayInsideValue === 'flex') {
-        // flex child should not shrink by default
-        nativeProps.style.flexShrink ??= 1;
-      }
-
-      // Use Animated components if necessary
-      if (nativeProps.animated === true) {
-        if (NativeComponent === ReactNative.ViewNativeComponent) {
-          NativeComponent = ReactNative.Animated.View;
-        }
-        if (NativeComponent === ReactNative.Pressable) {
-          NativeComponent = AnimatedPressable;
-        }
-      }
-
-      /**
-       * Construct tree
-       */
-
-      let element: React.Node =
-        typeof props.children === 'function' ? (
-          props.children(nativeProps)
-        ) : (
-          // $FlowFixMe
-          <NativeComponent {...nativeProps} />
-        );
-
-      // Enable W3C layout support
-      if (props['data-layoutconformance'] === 'strict') {
-        element = (
-          <ReactNative.LayoutConformance children={element} mode="strict" />
-        );
-      }
-
-      if (
-        (nativeProps.children != null &&
-          typeof nativeProps.children !== 'string') ||
-        typeof props.children === 'function'
-      ) {
-        if (inheritableStyle != null) {
-          element = (
-            <ProvideInheritedStyles
-              children={element}
-              value={inheritableStyle}
-            />
-          );
-        }
-        if (nextDisplayInsideValue !== displayInsideValue) {
-          element = (
-            <ProvideDisplayInside
-              children={element}
-              value={nextDisplayInsideValue}
-            />
-          );
-        }
-        if (customProperties != null) {
-          element = (
-            <ProvideCustomProperties
-              children={element}
-              value={customProperties}
-            />
-          );
-        }
-      }
-
-      if (hasTextAncestor) {
-        return (
-          <ReactNative.TextAncestorContext.Provider value={false}>
-            {element}
-          </ReactNative.TextAncestorContext.Provider>
-        );
-      }
-
-      return element;
+    if (
+      nativeProps.onPress != null &&
+      NativeComponent === ReactNative.ViewNativeComponent
+    ) {
+      NativeComponent = ReactNative.Pressable;
     }
-  );
 
-  component.displayName = `html.${tagName}`;
-  return component;
+    // Tag-specific props
+
+    if (tagName === 'button') {
+      nativeProps.role ??= 'button';
+    } else if (tagName === 'header') {
+      nativeProps.role ??= 'header';
+    } else if (tagName === 'li') {
+      nativeProps.role ??= 'listitem';
+    } else if (tagName === 'ol' || tagName === 'ul') {
+      nativeProps.role ??= 'list';
+    }
+
+    // Component-specific props
+
+    if (NativeComponent === ReactNative.Pressable) {
+      if (props.disabled === true) {
+        nativeProps.disabled = true;
+        nativeProps.focusable = false;
+      }
+    }
+
+    nativeProps.ref = elementRef;
+
+    // Workaround: React Native doesn't support raw text children of View
+    // Sometimes we can auto-fix this
+    if (typeof nativeProps.children === 'string') {
+      nativeProps.children = <TextString children={nativeProps.children} />;
+    }
+
+    // Polyfill for default of "display:block"
+    // which implies "displayInside:flow"
+    let nextDisplayInsideValue = 'flow';
+    const displayInsideValue = useDisplayInside();
+    const displayValue = nativeProps.style.display;
+
+    if (__DEV__) {
+      const nativeStyle = nativeProps.style;
+      if (displayInsideValue !== 'flex') {
+        // Error message if the element is not a flex child but tries to use flex
+        ['flex', 'flexBasis', 'flexGrow', 'flexShrink'].forEach((styleProp) => {
+          const value = nativeStyle[styleProp];
+          if (value != null) {
+            errorMsg(
+              `"display:flex" is required on the parent for "${styleProp}" to have an effect.`
+            );
+          }
+        });
+      }
+    }
+
+    if (displayValue === 'flex') {
+      nextDisplayInsideValue = 'flex';
+      nativeProps.style.alignContent ??= 'stretch';
+      nativeProps.style.alignItems ??= 'stretch';
+      nativeProps.style.flexBasis ??= 'auto';
+      nativeProps.style.flexDirection ??= 'row';
+      nativeProps.style.flexShrink ??= 1;
+      nativeProps.style.flexWrap ??= 'nowrap';
+      nativeProps.style.justifyContent ??= 'flex-start';
+    } else if (displayValue === 'block' && displayInsideValue === 'flow') {
+      // Force the block emulation styles
+      nextDisplayInsideValue = 'flow';
+      nativeProps.style.alignItems = 'stretch';
+      nativeProps.style.display = 'flex';
+      nativeProps.style.flexBasis = 'auto';
+      nativeProps.style.flexDirection = 'column';
+      nativeProps.style.flexShrink = 0;
+      nativeProps.style.flexWrap = 'nowrap';
+      nativeProps.style.justifyContent = 'flex-start';
+    }
+
+    if (displayInsideValue === 'flex') {
+      // flex child should not shrink by default
+      nativeProps.style.flexShrink ??= 1;
+    }
+
+    // Use Animated components if necessary
+    if (nativeProps.animated === true) {
+      if (NativeComponent === ReactNative.ViewNativeComponent) {
+        NativeComponent = ReactNative.Animated.View;
+      }
+      if (NativeComponent === ReactNative.Pressable) {
+        NativeComponent = AnimatedPressable;
+      }
+    }
+
+    /**
+     * Construct tree
+     */
+
+    let element: React.Node =
+      typeof props.children === 'function' ? (
+        props.children(nativeProps)
+      ) : (
+        // $FlowFixMe
+        <NativeComponent {...nativeProps} />
+      );
+
+    // Enable W3C layout support
+    if (props['data-layoutconformance'] === 'strict') {
+      element = (
+        <ReactNative.LayoutConformance children={element} mode="strict" />
+      );
+    }
+
+    if (
+      (nativeProps.children != null &&
+        typeof nativeProps.children !== 'string') ||
+      typeof props.children === 'function'
+    ) {
+      if (inheritableStyle != null) {
+        element = (
+          <ProvideInheritedStyles children={element} value={inheritableStyle} />
+        );
+      }
+      if (nextDisplayInsideValue !== displayInsideValue) {
+        element = (
+          <ProvideDisplayInside
+            children={element}
+            value={nextDisplayInsideValue}
+          />
+        );
+      }
+      if (customProperties != null) {
+        element = (
+          <ProvideCustomProperties
+            children={element}
+            value={customProperties}
+          />
+        );
+      }
+    }
+
+    if (hasTextAncestor) {
+      return (
+        <ReactNative.TextAncestorContext.Provider value={false}>
+          {element}
+        </ReactNative.TextAncestorContext.Provider>
+      );
+    }
+
+    return element;
+  }
+
+  Component.displayName = `html.${tagName}`;
+  return Component;
 }
--- packages/react-strict-dom/src/native/modules/createStrictDOMImageComponent.js
+++ packages/react-strict-dom/src/native/modules/createStrictDOMImageComponent.js
@@ -20,106 +20,104 @@ export function createStrictDOMImageComponent<P: StrictReactDOMImageProps, T>(
   tagName: string,
   _defaultProps?: P
 ): component(ref?: React.RefSetter<T>, ...P) {
-  const component: React.AbstractComponent<P, T> = React.forwardRef(
-    function (props, forwardedRef) {
-      let NativeComponent = ReactNative.Image;
-      const elementRef = useStrictDOMElement<T>(forwardedRef, { tagName });
+  component Component(ref: React.RefSetter<T>, ...props: P) {
+    let NativeComponent = ReactNative.Image;
+    const elementRef = useStrictDOMElement<T>(ref, { tagName });
 
-      const {
-        alt,
-        crossOrigin,
-        height,
-        onError,
-        onLoad,
-        referrerPolicy,
-        src,
-        srcSet,
-        width
-      } = props;
+    const {
+      alt,
+      crossOrigin,
+      height,
+      onError,
+      onLoad,
+      referrerPolicy,
+      src,
+      srcSet,
+      width
+    } = props;
 
-      /**
-       * Resolve global HTML and style props
-       */
+    /**
+     * Resolve global HTML and style props
+     */
 
-      const defaultProps = {
-        style: [
-          _defaultProps?.style,
-          height != null && width != null && styles.aspectRatio(width, height)
-        ]
-      };
+    const defaultProps = {
+      style: [
+        _defaultProps?.style,
+        height != null && width != null && styles.aspectRatio(width, height)
+      ]
+    };
 
-      const { nativeProps } = useNativeProps(defaultProps, props, {
-        provideInheritableStyle: false,
-        withInheritedStyle: false,
-        withTextStyle: false
-      });
+    const { nativeProps } = useNativeProps(defaultProps, props, {
+      provideInheritableStyle: false,
+      withInheritedStyle: false,
+      withTextStyle: false
+    });
 
-      // Tag-specific props
+    // Tag-specific props
 
-      if (alt != null) {
-        nativeProps.alt = alt;
-      }
-      if (crossOrigin != null) {
-        nativeProps.crossOrigin = crossOrigin;
-      }
-      if (height != null) {
-        nativeProps.height = height;
-      }
-      if (onError != null) {
-        nativeProps.onError = function () {
-          onError({
-            type: 'error'
-          });
-        };
-      }
-      if (onLoad != null) {
-        nativeProps.onLoad = function (e) {
-          const { source } = e.nativeEvent;
-          onLoad({
-            target: {
-              naturalHeight: source?.height,
-              naturalWidth: source?.width
-            },
-            type: 'load'
-          });
-        };
-      }
-      if (referrerPolicy != null) {
-        nativeProps.referrerPolicy = referrerPolicy;
-      }
-      if (src != null) {
-        nativeProps.src = src;
-      }
-      if (srcSet != null) {
-        nativeProps.srcSet = srcSet;
-      }
-      if (width != null) {
-        nativeProps.width = width;
-      }
-
-      // Component-specific props
-
-      nativeProps.ref = elementRef;
-
-      // Use Animated components if necessary
-      if (nativeProps.animated === true) {
-        NativeComponent = ReactNative.Animated.Image;
-      }
-
-      const element: React.Node =
-        typeof props.children === 'function' ? (
-          props.children(nativeProps)
-        ) : (
-          // $FlowFixMe
-          <NativeComponent {...nativeProps} />
-        );
-
-      return element;
+    if (alt != null) {
+      nativeProps.alt = alt;
     }
-  );
+    if (crossOrigin != null) {
+      nativeProps.crossOrigin = crossOrigin;
+    }
+    if (height != null) {
+      nativeProps.height = height;
+    }
+    if (onError != null) {
+      nativeProps.onError = function () {
+        onError({
+          type: 'error'
+        });
+      };
+    }
+    if (onLoad != null) {
+      nativeProps.onLoad = function (e) {
+        const { source } = e.nativeEvent;
+        onLoad({
+          target: {
+            naturalHeight: source?.height,
+            naturalWidth: source?.width
+          },
+          type: 'load'
+        });
+      };
+    }
+    if (referrerPolicy != null) {
+      nativeProps.referrerPolicy = referrerPolicy;
+    }
+    if (src != null) {
+      nativeProps.src = src;
+    }
+    if (srcSet != null) {
+      nativeProps.srcSet = srcSet;
+    }
+    if (width != null) {
+      nativeProps.width = width;
+    }
 
-  component.displayName = `html.${tagName}`;
-  return component;
+    // Component-specific props
+
+    nativeProps.ref = elementRef;
+
+    // Use Animated components if necessary
+    if (nativeProps.animated === true) {
+      NativeComponent = ReactNative.Animated.Image;
+    }
+
+    const element: React.Node =
+      typeof props.children === 'function' ? (
+        props.children(nativeProps)
+      ) : (
+        // $FlowFixMe
+        <NativeComponent {...nativeProps} />
+      );
+
+    return element;
+  }
+
+  Component.displayName = `html.${tagName}`;
+  return Component;
 }
 
 const styles = css.create({
