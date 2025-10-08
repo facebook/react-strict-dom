721d34e5b9f84cf5e54e82359eb02e9eb67bb50c
Nicolas Gallagher <necolas@meta.com>
2025-10-07 20:58:01 -0700

    Add llms.txt
    Close #386

--- packages/website/docusaurus.config.js
+++ packages/website/docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/react-strict-dom/',
-  trailingSlash: true,
+  trailingSlash: undefined,
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   // GitHub pages deployment config.
